1c8bd5dc2cbc1be1dcd00900de9c30d13d7e164c
ppoupee <ppoupeefr@yahoo.fr>
2024-10-14 16:41:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,8 @@ if ingredient_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! '+ name_on_order, icon="✅")
-
+# New session to display nutrition info
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+#st.text(fruityvice_response.json())
