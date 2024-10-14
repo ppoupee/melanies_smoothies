1b0d39ab8621312966eae0aa82152a13a1054f5a
ppoupee <ppoupeefr@yahoo.fr>
2024-10-14 16:34:30 +0200

    Update streamlit_app.py
    add json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ if ingredient_list:
         st.success('Your Smoothie is ordered! '+ name_on_order, icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
