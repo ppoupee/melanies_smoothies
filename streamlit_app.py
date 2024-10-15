1260d51ca6518fd8e86596376024623ebf2dfc56
ppoupee <ppoupeefr@yahoo.fr>
2024-10-15 14:28:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,6 @@ if ingredient_list:
 # New session to display nutrition info
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
-st.text(fruityvice_response.json())
+#st.write(fruityvice_response)
+#st.text(fruityvice_response.json())
+#fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
